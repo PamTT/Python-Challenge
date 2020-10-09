8a3748d3cf747c8ab0cfe40cb95ba21f5fc457a5
PAM tameerug <pam.tameerug@yahoo.com>
2020-10-09 13:40:08 -0500

    import budget_data.csv file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,25 @@
 #MainPy
+# First we'll import the os module
+import os
+# This will allow us to create file paths across operating systems
+# Module for reading CSV files
+import csv
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+
+# Calculate the total number of months included in the dataset
+
+
+#Calculate the net total amount of "Profit/Losses" over the entire period
+
+
+#Calculate the average of the changes in "Profit/Losses" over the entire period
+
+
+#Calcurate the greatest increase in profits (date and amount) over the entire period
+
+
+#Calcurate the greatest decrease in losses (date and amount) over the entire period
+
+# print
+
+# export file as a result
