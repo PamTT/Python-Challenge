72e384f3f6080387a3c795f0ae56b7d38d10facc
PAM tameerug <pam.tameerug@yahoo.com>
2020-10-10 20:41:01 -0500

    added total_month  total_profit/losses  average

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,18 +1,43 @@
 #MainPy
-# First we'll import the os module
+
 import os
-# This will allow us to create file paths across operating systems
-# Module for reading CSV files
 import csv
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('Resources', 'budget_data.csv')
 
-# Calculate the total number of months included in the dataset
-
+#Calculate the total number of months included in the dataset
+number_of_months=0
+with open (csvpath,'r',) as csv_file:
+    csv_reader_object = csv.reader(csv_file)
+    csv_reader = csv.reader(csv_file,delimiter=',')
+    for row in csv_reader:
+        print("csv row: {0}".format(row))
+        number_of_months = number_of_months+1
+        csv_reader_object.line_num
+print('total number of months ' + str(number_of_months-1))
 
 #Calculate the net total amount of "Profit/Losses" over the entire period
+months = 86
+profits_losses=[]
+with open (csvpath,'r',) as csv_file:
+    for row in csv.reader(csv_file):
+        profits_losses.append(row[1])
+    profits_losses.remove("Profit/Losses")
+
+    for i in range(0,len(profits_losses)):
+         profits_losses[i] =int(profits_losses[i])   
+
+print(profits_losses)
+
+net_profit=sum(profits_losses)
+average_change = net_profit/number_of_months
+
+print('Total amount of Profit/losses is ' + str(net_profit))
+print('Average Profit/losses is ' +str(average_change))
 
 
 #Calculate the average of the changes in "Profit/Losses" over the entire period
+#average_change = profits_losses/months
+#print(average_change)
 
 
 #Calcurate the greatest increase in profits (date and amount) over the entire period
@@ -23,3 +48,5 @@ csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 # print
 
 # export file as a result
+
+
